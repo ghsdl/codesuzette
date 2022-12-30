6322d4ccd9dbd5a0e950e69949ab38663f858e2e
ghsdl <maeva.ghsdl@gmail.com>
2022-12-30 22:45:22 +0100

    :necktie: design about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,30 +1,15 @@
 import Paragraph from '../Paragraph/Paragraph';
 import Picture from '../Picture/Picture';
 import Subtitle from '../Subtitle/Subtitle';
-import AboutCard from '../DoubleFace/DoubleFace';
+import DoubleFace from '../DoubleFace/DoubleFace';
 import CREPE from '../../assets/images/CREPE.svg';
-import AnimatedIcon from '../AnimatedIcon/AnimatedIcon';
-import emoji from '../../assets/animations/emoji.json';
 import './About.scss';
 
 const About = () => {
   return (
     <div className="about">
       <div className="about__header">
-        <AnimatedIcon
-          isStopped={false}
-          width={50}
-          height={50}
-          options={{
-            loop: true,
-            autoplay: true,
-            animationData: emoji,
-            rendererSettings: {
-              preserveAspectRatio: 'xMidYMid slice'
-            }
-          }}
-          className="about__emoji"
-        />
+        <Picture src={CREPE} alt="logo" className="about__logo" />
         <Subtitle>Heeeeey!</Subtitle>
       </div>
       <div className="about__mission">
@@ -32,20 +17,19 @@ const About = () => {
           Notre mission est de faciliter la recherche de ressources sur le
           développement web.
         </Paragraph>
-        <Picture src={CREPE} alt="logo" className="about__logo" />
       </div>
       <section className="about__cards">
-        <AboutCard
+        <DoubleFace
           title="Behind"
           label="Le projet est né d'une volonté d'avoir au même endroit des ressources dans lesquelles piocher selon le besoin ou pour explorer."
           isRed={true}
         />
-        <AboutCard
+        <DoubleFace
           title="the"
           label="La mission première du site sera toujours de faciliter l'accès à des ressources sur le développement web pour apprendre et s'entraîner."
           isLightPurple={true}
         />
-        <AboutCard
+        <DoubleFace
           title="scenes"
           label="Code Suzette est un projet perso gratuit et maintenu autant que possible selon le temps libre de sa créatrice."
           isGreen={true}
